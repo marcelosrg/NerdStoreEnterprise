45a4d8de2061cc4cc3e7ddc792fc66e0260c8511
marcelosrg <marcelohrs2005@gmail.com>
2025-02-15 22:25:25 -0300

    feat: implementando tratamento de erros na api

---
+++ src/services/NSE.Identidade.API/Controllers/ControllerBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public class ControllerBase : Controller
+    {
+        protected ICollection<string> Errors = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperationValid())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Mensagens", Errors.ToArray() }
+            }));
+
+        }
+
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach(var error in errors)
+            {
+                AddErrorProcess(error.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool OperationValid()
+        {
+            return !Errors.Any();
+        }
+
+        protected void AddErrorProcess(string error)
+        {
+            Errors.Add(error);
+        }
+        protected void RemoveErrorProcess(string error)
+        {
+            Errors.Clear();
+        }
+    }
+}
