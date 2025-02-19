afc97be5bbe45a9239059ea1e7806ee83eded700
marcelosrg <marcelohrs2005@gmail.com>
2025-02-18 21:39:00 -0300

    feat: implementando rota para criar produtos

--- src/services/NSE.Catalogo.API/Controllers/CatalogController.cs
+++ src/services/NSE.Catalogo.API/Controllers/CatalogController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using NSE.Catalogo.API.Models;
+using NSE.WebAPI.Core.Identity;
 using System.Threading.Tasks;
 
 namespace NSE.Catalogo.API.Controllers
 {
 
     [ApiController]
+    [Authorize]
     public class CatalogController : ControllerBase
     {
 
@@ -17,21 +20,45 @@ namespace NSE.Catalogo.API.Controllers
             _productRepository = productRepository;
         }
 
-
+        [AllowAnonymous]
         [HttpGet("catolo/produtos")]
         public async Task<IEnumerable<Product>> Index()
         {
             return await _productRepository.GetAllProducts();
         }
 
+        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catolo/produtos/{id}")]
         public async Task<Product> GetDetailsProduct(Guid id)
         {
             return await _productRepository.GetProductById(id);
         }
 
+        [AllowAnonymous]
+        [HttpPost("catolo/produtos/create")]
+        public  void CreateProduct(Product product)
+        {
+            var createProduct = new Product
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Active = product.Active,
+                CreatedAt = DateTime.UtcNow,
+                Descripition = product.Descripition,
+                Image = product.Image,
+                StockQuantity = product.StockQuantity
 
-     
+
+            };
+
+             _productRepository.AddProduct(product);
+
+        }
+
+
+
+
+
 
     }
 }
