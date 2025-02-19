89cbd296b46d5d9e7e312de7416f6046e6c905b9
marcelosrg <marcelohrs2005@gmail.com>
2025-02-18 22:05:50 -0300

    feat: adicionando autenticação nas rotas da api de catalogo

---
+++ src/building blocks/NSE.WebAPI.Core/Identity/CustomAuthorization.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace NSE.WebAPI.Core.Identity
+{
+    class CustomAuthorization
+    {
+        public static bool ValidateClaimsUser(HttpContext context, string claimName, string claimValue)
+        {
+            return context.User.Identity.IsAuthenticated &&
+                    context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+
+    }
+}
